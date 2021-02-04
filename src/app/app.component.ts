5ca16995fdf7f494a0694f1b78d94f7cced913e8
jcmann <jen.c.mann@gmail.com>
2021-02-04 15:38:24 -0600

    Added input for custom quizzes, and delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'Quiz Editor';
+    title = 'Quiz Editor';
 
-  addFunnyQuiz = () => {
-      this.quizzes = [
-          ...this.quizzes, 
-          {
-              name: "Funny Quiz", 
-              showDelete: false,
-          }
-      ]; 
-  }
-
-  quizzes = [
+    quizzes = [
         {
             name: "Quiz 1", 
             showDelete: false
@@ -31,5 +23,38 @@ export class AppComponent {
             name: "Quiz 3", 
             showDelete: false 
         }
-  ]; 
+    ]; 
+
+    addFunnyQuiz = () => {
+        this.quizzes = [
+        ...this.quizzes, 
+            {
+                name: "Funny Quiz", 
+                showDelete: false,
+            }
+        ]; 
+    }
+
+    myNewQuizName = ""; // because typescript is typed!
+
+    addQuiz = () => {
+        this.quizzes = [
+            ...this.quizzes, 
+            {
+                name: this.myNewQuizName, 
+                showDelete: true
+            }
+        ]; 
+
+        this.myNewQuizName = ""; 
+    }
+
+    deleteQuiz = (quizToDelete) => {
+
+        this.quizzes = [
+            ...this.quizzes.filter(x => x != quizToDelete)
+        ]
+
+    }
+
 }
